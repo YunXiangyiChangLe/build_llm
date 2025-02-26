3830dd1ec8633ce2b3d395648ad0a00473f7debe
shaozeguang <shaozeguang@tju.edu.cn>
2025-02-26 17:59:08 +0800

    self-atten优化

--- self_attention.py
+++ self_attention.py
@@ -3,18 +3,18 @@ import torch.nn as nn
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, d_in, d_out):
+    def __init__(self, d_in, d_out, qkv_bias=False):
         super().__init__()
         self.d_out = d_out
         self.d_in = d_in
-        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
-        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
-        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
 
     def forward(self, x):
-        keys = x @ self.W_key
-        queries = x @ self.W_query
-        values = x @ self.W_value
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
         atten_sorces = queries @ keys.T
         atten_weights = torch.softmax(atten_sorces / keys.shape[-1] ** 0.5, dim=-1)
         context_vecs = atten_weights @ values
