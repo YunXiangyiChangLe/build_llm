8c1bfb275583339cfbe5ce41afc1f58c58107d1f
shaozeguang <shaozeguang@tju.edu.cn>
2025-02-27 18:03:35 +0800

    gelu ffd

--- gpt_model.py
+++ gpt_model.py
@@ -34,8 +34,39 @@ class TransformerBlock(nn.Module):
 
 
 class LayerNorm(nn.Module):
-    def __init__(self, normalized_shape, eps=1e-5):
+    def __init__(self, normalized_shape, emb_dim, eps=1e-5):
+        super().__init__()
+        self.eps = eps
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return x * self.scale + self.shift
+
+
+class GELU(nn.Module):
+    def __init__(self):
         super().__init__()
 
     def forward(self, x):
-        return x
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * (
+                    x + 0.044715 * torch.pow(x, 3)
+            )
+        ))
+
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"])
+        )
+
+    def forward(self, x):
+        return self.layers(x)
